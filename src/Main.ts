183f962e78cbbe2372f0253887a60b53bb3589c3
yongsy-17 <yongsy.din@student.passerellesnumeriques.org>
2025-05-31 12:29:24 +0700

    fix main

--- src/Main.ts
+++ src/Main.ts
@@ -4,7 +4,7 @@ import { DeliveryManager } from "./Deliver/DeliveryManager";
 import { DeliveryType } from "./Deliver/DeliveryType";
 //check the delivery method and destination of each shipment
 const expressOption = new DeliveryOption(DeliveryType.EXPRESS, 10);
-const shipment1 = new Shipment("TRK123456", "Phnom Penh", expressOption);
+const shipment1 = new Shipment("370 street", "Phnom Penh", expressOption);
 
 const manager = new DeliveryManager(1, "Sokha");
 const details = manager.getShipmentDetails(shipment1);
